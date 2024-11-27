1f9b99dfdb98e5d529abb445e34cb84470d9b77e
zosia <z.akrzewsk.a.z.a@gmail.com>
2024-11-27 21:14:11 +0100

    Dodano DAGi do pobierania i procesowania danych

--- dags/download_split_data_dag.py
+++ dags/download_split_data_dag.py
@@ -1,40 +1,55 @@
+import os
+
 from airflow import DAG
 from _datetime import datetime
 
 from airflow.operators.python import PythonOperator
 from google.oauth2.service_account import Credentials
 from sklearn.model_selection import train_test_split
+import pandas as pd
 import gspread
 
-from model.clean_split_data import get_data
+# Przy odpalaniu DAGa w Airflow dostawalam segmentation fault - to jest workaround,
+# z tego co wyczytalam nie jest to wina kodu a interakcji bibliotek z ARM i proxy
+os.environ["no_proxy"] = "*"
+
+
+def get_data(file_path):
+    return pd.read_csv(file_path)
 
 
 def download_data(**kwargs):
-    dataframe = get_data("../dataset/StudentPerformanceFactors.csv")
+    dataframe = get_data("/Users/zosia/uczelnia/4_rok/1_sem/studentScores/dags/StudentPerformanceFactors.csv")
+    dataframe = dataframe.dropna()
     return dataframe.to_json()
 
 
 def split_data(**kwargs):
-    dataframe = kwargs['ti']
+    dataframe = pd.read_json(kwargs["ti"].xcom_pull(task_ids="download_data"))
     basic_train_data, additional_train_data = train_test_split(dataframe, test_size=0.3, random_state=42)
 
-    return {
-        "basic_train_data": basic_train_data.to_json(),
-        "additional_train_data": additional_train_data.to_json()
-    }
+    kwargs["ti"].xcom_push(key="basic_train_data", value=basic_train_data.to_json())
+    kwargs["ti"].xcom_push(key="additional_train_data", value=additional_train_data.to_json())
 
 
 def upload_data(**kwargs):
-    upload_data_to_google_sheets("Basic train data", kwargs['ti'].xcom_pull(key="basic_train_data"))
-    upload_data_to_google_sheets("Additional train data", kwargs['ti'].xcom_pull(key="additional_train_data"))
+    upload_data_to_google_sheets("Basic_train_data", kwargs["ti"]
+                                 .xcom_pull(key="basic_train_data", task_ids="split_data"))
+    upload_data_to_google_sheets("Additional_train_data", kwargs["ti"]
+                                 .xcom_pull(key="additional_train_data", task_ids="split_data"))
 
 
-def upload_data_to_google_sheets(sheet_name, dataframe):
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = Credentials.from_service_account_file('/opt/airflow/dags/secret.json', scope=scope)
+def upload_data_to_google_sheets(worksheet_name, data):
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    credentials = Credentials.from_service_account_file(
+        "/Users/zosia/uczelnia/4_rok/1_sem/studentScores/secrets/asiprojekt-8ff60650e1f0.json"
+    ).with_scopes(scope)
+
     client = gspread.authorize(credentials)
+    sheet = client.open("ASI_2024_Projekt").worksheet(worksheet_name)
 
-    sheet = client.open("ASI 2024 Projekt").worksheet()
+    dataframe = pd.read_json(data)
+    sheet.update([dataframe.columns.values.tolist()] + dataframe.values.tolist())
 
 
 with DAG("download_split_data_dag",
